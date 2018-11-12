f47129ada126408a686783e1aba0d65ed34ee0cb
Ashwani Jha <ashwanijha04@gmail.com>
2018-11-13 00:35:06 +0530

    Update crc.c

--- crc.c
+++ crc.c
@@ -1,66 +1,88 @@
 #include<stdio.h>
 #include<string.h>
-#define N strlen(g)
+#define N strlen(divisor)
 
-char t[28],cs[28],g[16];
-int a,e,c;
+// t : data
+// cs : CheckSum 
+// a = length of databits
+// g : divisor
+// N : Divisor Length
+
+
+char data[28],checksum[28],divisor[16];
+int datalength,error;
+int i;
 
 void xor(){
-    for(c = 1;c < N; c++)
-    cs[c] = (( cs[c] == g[c])?'0':'1');
+    for(i = 1; i< N; i++)
+    checksum[i] = (( checksum[i] == divisor[i])?'0':'1');
 }
 
 void crc(){
-    for(e=0;e<N;e++)
-        cs[e]=t[e];
+    for(error=0; error<N; error++)
+        checksum[error]=data[error];
+    
     do{
-        if(cs[0]=='1')
+        if(checksum[0]=='1')
             xor();
-        for(c=0;c<N-1;c++)
-            cs[c]=cs[c+1];
-        cs[c]=t[e++];
-    }while(e<=a+N-1);
+        for(i=0; i<N-1; i++)
+            checksum[i]=checksum[i+1];
+        checksum[i]=data[error++];
+    }while(error<=datalength+N-1);
 }
 
 int main()
 {
     printf("\nEnter data : ");
-    scanf("%s",t);
+    scanf("%s",data);
     printf("\n----------------------------------------");
+
     printf("\nEnter divisor: ");
-    scanf("%s", g);
-    a = strlen(t);
-    for(e=a;e<a+N-1;e++)
-        t[e]='0';
+    scanf("%s", divisor);
+
+    datalength = strlen(data);
+
+    for(error=datalength;error<datalength+N-1;error++)
+        data[error]='0';
+
     printf("\n----------------------------------------");
-    printf("\nModified data is : %s",t);
+    printf("\nModified data is : %s",data);
     printf("\n----------------------------------------");
+
+
     crc();
-    printf("\nChecksum is : %s",cs);
-    for(e=a;e<a+N-1;e++)
-        t[e]=cs[e-a];
+
+
+    printf("\nChecksum is : %s",checksum);
+
+    for(error=datalength;error<datalength+N-1;error++)
+        data[error]=checksum[error-datalength];
+
     printf("\n----------------------------------------");
-    printf("\nFinal codeword is : %s",t);
+    printf("\nFinal codeword is : %s",data);
     printf("\n----------------------------------------");
     printf("\nTest error detection 0(yes) 1(no)? : ");
-    scanf("%d",&e);
-    if(e==0)
+    scanf("%d",&error);
+
+    if(error==0)
     {
         do{
             printf("\nEnter the position where error is to be inserted : ");
-            scanf("%d",&e);
-        }while(e==0 || e>a+N-1);
-        t[e-1]=(t[e-1]=='0')?'1':'0';
+            scanf("%d",&error);
+        }while(error==0 || error>datalength+N-1);
+
+        data[error-1]=(data[error-1]=='0')?'1':'0';
         printf("\n----------------------------------------");
-        printf("\nErroneous data : %s\n",t);
+        printf("\nErroneous data : %s\n",data);
     }
+
     crc();
-    for(e=0;(e<N-1) && (cs[e]!='1');e++)
+    for(error=0;(error<N-1) && (checksum[error]!='1');error++)
      ;
-        if(e<N-1)
-            printf("\nError detected, syndrome bits are: %s\n\n", cs);
+        if(error<N-1)
+            printf("\nError detected, syndrome bits are: %s\n\n", checksum);
         else
-            printf("\nNo error detected, syndrome bits are: %s\n\n", cs);
+            printf("\nNo error detected, syndrome bits are: %s\n\n", checksum);
             printf("\n----------------------------------------\n");
         return 0;
 }
